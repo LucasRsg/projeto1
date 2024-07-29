464bcc28ceaa662d566e8ae2a8b6897cb565af37
Lucas do Rosário <lucasrsgoncalves@gmail.com>
2024-07-29 20:37:17 -0300

    subindo

--- src/pages/app/index.jsx
+++ src/pages/app/index.jsx
@@ -1,5 +1,5 @@
 
-import './index.css';
+import './index.scss';
 
 import { Link } from 'react-router-dom';
 
@@ -7,15 +7,26 @@ export default App;
 
 function App() {
   return (
-    <div className="App">
-      <h1>Estudos de ReactJS</h1>
-      <br/>
-      <i className='fa fa-heart'></i>
-      <h1>Salve</h1>
-      <ul>
-        <li>
+    <div className="pagina-app pagina">
+      <header className='cabecalho'>
+
+      <h1 className='titulo1'>Estudos de ReactJS<i className='fa fa-heart'></i></h1>
+
+      </header>
+
+    <section className='secao'>
+      <h1>
+        Estudando Reactjs
+      </h1>
+
+      <li>
 <Link to='/contato'>Ir para o contato </Link>
         </li>
+
+    </section>
+
+      <ul>
+
       </ul>
     </div>
   );
--- src/pages/contato/index.jsx
+++ src/pages/contato/index.jsx
@@ -1,4 +1,4 @@
-import './index.css'
+import './index.scss'
 
 import { Link } from 'react-router-dom';
 
@@ -7,13 +7,17 @@ export default function Contato(){
 
     return(
     
-        <div className='pagina-contato'>
+        <div className='pagina-contato pagina'>
+            <header className='cabecalho'>
             <Link to='/'>
             <i className='fa fa-arrow-left voltar'/>
             </Link>
-            <h1>Contato</h1>
-            
+            <h1 className='titulo2'>Contato</h1>
+            </header>
+            <section className='secao'>
+                <h1>Entre em contato !</h1>
             <img className='icone 'src='/assets/images/salve.png' />
+            </section>
         </div>
     )
 }
